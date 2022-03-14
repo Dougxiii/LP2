70c330bc765299d9722150277b14f7b75939f09c
Douglas Barcellos <84218639+Dougxiii@users.noreply.github.com>
2022-03-13 21:00:18 -0300

    Create figure.java

---
+++ 2.6.3/figures/figure.java
@@ -0,0 +1,19 @@
+package figures;
+
+import java.awt.*;
+
+public abstract class Figure{
+    public int x,y;
+    public int w,h;
+    public Color corFundo, corContorno;
+    
+    public Figure (int x, int y, int w, int h, Color corFundo, Color corContorno) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.corFundo = corFundo;
+        this.corContorno = corContorno;
+    }
+    public abstract void paint (Graphics g);
+}
